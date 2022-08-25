ef1d54cdbd9ec45e9733d9da79b2ada017fdf6bc
DELL <info.yassinesouri@gmail.com>
2022-08-25 14:17:25 +0100

    initSpringMVC

---
+++ src/main/java/com/yassine/controller/Test.java
@@ -0,0 +1,14 @@
+package com.yassine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+
+    @RequestMapping("/")
+    public String display()
+    {
+        return "home";
+    }
+}
